db631a21ffb161ac500927f4eb03c100f45a1352
Ansh Lodhi <ansh.lodhi@accordion.com>
2024-11-17 00:14:30 +0530

    client data scrapper

--- backend/pipeline1.py
+++ backend/pipeline1.py
@@ -1,8 +1,9 @@
 
-from langchain.llms import OpenAI
 from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
 from langchain.chat_models import ChatOpenAI
+from crewai import Agent, Task, Crew, Process
+from crewai_tools import SerperDevTool, ScrapeWebsiteTool
 import json
 import os
 
@@ -64,9 +65,71 @@ def analyze_transcript(transcript: str, config) -> str:
     return response
 
 
-def client_info_fun(client_name):
+def client_info_fun(client_name: str,config, **client_details):
+    """
+    Function to generate a comprehensive summary of a company's business profile.
+    
+    Parameters:
+    - company_name (str): The name of the company.
+    - additional_details (dict): Additional dynamic parameters like key performance indicators or areas of focus.
 
-    return 'Test Client Info'
+    Returns:
+    - str: The generated company summary with key metrics, strategic focus areas, and more.
+    """
+
+
+ 
+
+    # Required following parameters. Update following input parameters. Make sure embedding model and LLM models are deployed within same resource.
+
+    openai_key=config["OPENAI_API_KEY"]
+    serper_api_key = config["serper_api_key"]
+    
+
+    ## Following is very essential to run crewAI agent. Environment key OPENAI_API_KEY must be set up.
+    os.environ['SERPER_API_KEY']= serper_api_key
+    os.environ["OPENAI_API_KEY"]= openai_key
+    llm_crew = ChatOpenAI(api_key=openai_key,model="gpt-4o-mini")
+
+    # Check if the API keys are set
+    if not openai_key or not serper_api_key:
+        raise ValueError("API keys are missing. Please set the OPENAI_API_KEY and SERPER_API_KEY environment variables.")
+
+
+    # Define the Agents
+    internet_research_agent = Agent(
+    role = "Internet Research Agent",
+    goal = "Your goal is to assist in identifying and analyzing strategic business insights from trusted web sources. You will focus on uncovering market trends, key metrics (quantative ), competitive positioning, and innovation opportunities to help the business development team craft tailored solutions and value propositions.",
+    backstory = "You are a business-focused research agent with expertise in uncovering actionable business insights. Your role is to assist in the discovery of growth opportunities, competitive advantages, and pain points in the client's industry to support strategic decision-making and client proposals.",
+    allow_delegation = False,
+    verbose = True,
+    llm = ChatOpenAI(api_key=openai_key,model="gpt-4o-mini"))
+    
+    # Define tools for the tasks
+    tools = [SerperDevTool(), ScrapeWebsiteTool()]
+    
+    # Define Tasks for Agents
+    internet_research_task = Task(
+    description = f"Analyze the company {client_name}’s profile based on the provided query : {{question}} and generate insights on key business factors including market positioning, competitive advantages, target segments, financial performance, strategic priorities, and technological innovation. Extract information that will aid in identifying areas of opportunity or potential challenges for business development efforts.",
+    expected_output = "Provide a comprehensive analysis of the company, including its strategic position, industry trends, financial health, key growth areas, and technologies used. Highlight potential business pain points, opportunities for growth, and areas where competitive differentiation can be achieved. Include references to sources that support your findings.",
+    tools = tools,
+    agent = internet_research_agent,
+    )
+    
+    agents = [internet_research_agent]
+    tasks = [internet_research_task]
+    
+    crew = Crew(
+            agents = agents,
+            tasks = tasks,
+            process = Process.sequential,
+            verbose=True
+        )
+
+    # inputs = {"question":"Generate a summary of of client {client_name}, covering it's industry positioning, ownership, target market segment, latest available key metrics, strategic focus areas, and the primary technologies or tools they use."}   
+    inputs = {"question":f"Generate a summary of of client {client_name}, covering it's industry positioning, ownership, target market segment, latest available key metrics, strategic focus areas, and the primary technologies or tools they use."}
+    response = crew.kickoff(inputs=inputs)
+    return response.raw  
 
 def pipeline1(transcript, config):
     """
@@ -84,7 +147,7 @@ def pipeline1(transcript, config):
     client_data = json.loads(transcript_report[7:-3])
     client_name = client_data['Company Profile']['Company Name'] #get based on transcript response
 
-    client_background = client_info_fun(client_name)
+    client_background = client_info_fun(client_name, config)
 
     return transcript, transcript_report, client_background
 
